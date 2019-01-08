dd22a11f432c41a81345d156b53fd63ce1bfd0c8
ilbrigz <ilogirb23@yahoo.com>
2019-01-08 17:40:52 +0800

    Updates

---
+++ precache-manifest.afe8281c75f4baa7be13941daf2eaec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6fa9e998a2e14ff9d27",
+    "url": "/drumkit/static/js/runtime~main.f6fa9e99.js"
+  },
+  {
+    "revision": "f2602e338609b2bcef2d",
+    "url": "/drumkit/static/js/main.f2602e33.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/drumkit/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "f2602e338609b2bcef2d",
+    "url": "/drumkit/static/css/main.b3f60ddf.chunk.css"
+  },
+  {
+    "revision": "605a53113844038f7d018ffad256b427",
+    "url": "/drumkit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.962851e7e270c74b77ea71d79fa2e0c0.js"
+  "/drumkit/precache-manifest.afe8281c75f4baa7be13941daf2eaec4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/drumkit/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
