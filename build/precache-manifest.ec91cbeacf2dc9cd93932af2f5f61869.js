d8e9351c0eb6906b812298f8531b03fa2305321f
ilbrigz <ilogirb23@yahoo.com>
2019-01-08 14:09:07 +0800

    .

---
+++ build/precache-manifest.ec91cbeacf2dc9cd93932af2f5f61869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6fa9e998a2e14ff9d27",
+    "url": "/drumkit/static/js/runtime~main.f6fa9e99.js"
+  },
+  {
+    "revision": "161e119e22fc9bbdd91f",
+    "url": "/drumkit/static/js/main.161e119e.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/drumkit/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "161e119e22fc9bbdd91f",
+    "url": "/drumkit/static/css/main.b3f60ddf.chunk.css"
+  },
+  {
+    "revision": "6cb72e36318702667f8a18555fb3f0fb",
+    "url": "/drumkit/index.html"
+  }
+];
